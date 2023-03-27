fb4670791751fc2889cc7a6a0b13c3ba6484281d
alexlueng <alexlueng000@gmail.com>
2023-03-27 15:11:29 +0800

    add question_gpt tab

--- src/pages/ToolsPage.tsx
+++ src/pages/ToolsPage.tsx
@@ -2,10 +2,13 @@ import React, {ChangeEventHandler, useState } from 'react'
 import TextInput from '../components/TextInput'
 import TextOutput from '../components/TextOutput';
 
+const options = ['Golang', 'Deep Learning', 'Python']
+
 function ToolsPage() {
 
   const [activeTab, setActiveTab] = useState('tab1')
   const [inputText, setInputText] = useState("");
+  const [selectOption, setSelectOption] = useState(options[0]);
 
   const handleInputChange: ChangeEventHandler<HTMLTextAreaElement> = (event) => {
     const { value = "" } = event.target || {}
@@ -45,21 +48,22 @@ function ToolsPage() {
           >
               选择题
           </button>
-
+          <button className={`text-gray-600 py-4 px-6 block hover:text-blue-500 focus:outline-none ${activeTab === "tab3" ? "text-blue-500 border-b-2 font-medium border-blue-500" : ""}`}
+          onClick={() => setActiveTab("tab3")}
+          >
+              QuestionGPT
+          </button>
         </nav>
       </div>
 
       {activeTab === "tab1" && (
         <div className="bg-gray-100 py-4 px-2 flex flex-row h-screen">
-          {/* <h2 className="text-lg font-bold mb-2">Tab 1 Content</h2> */}
-          {/* <p>Here's some content for tab 1.</p> */}
           <div className='w-1/2 mr-2'>
             <TextInput onInputChange={handleInputChange} value={inputText} />
           </div>
           <div className='w-1/2 ml-2'>
             <TextOutput outputText={inputText} />
           </div>
-          
         </div>
       )}
 
@@ -69,6 +73,25 @@ function ToolsPage() {
           <p>Here's some content for tab 2.</p>
         </div>
       )}
+
+      {activeTab === "tab3" && (
+        <div className="bg-gray-100 py-4 px-2 h-screen">
+          <div  className='flex items-center justify-center'>
+            <label className='text-gray-700 text-sm font-bold mr-2' htmlFor='question'>Type:</label>
+            <select
+              value={selectOption}
+              onChange={(e) => setSelectOption(e.target.value)}
+            >
+              {options.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+      )}
+
 
     </div>
     
